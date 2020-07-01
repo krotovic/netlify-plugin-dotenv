1f4f431d0200e3002a921e1a8be63ae764db9e64
Lukáš Krotovič <lk.firefox@gmail.com>
2020-07-01 22:29:39 +0200

    add logging

--- src/index.js
+++ src/index.js
@@ -16,17 +16,24 @@ const resolveFile = (method) => {
     }
 };
 
+function log(message) {
+    if (!process.env.DEBUG) return;
+    console.debug(message);
+}
+
 module.exports = {
     onPreBuild({ inputs, constants }) {
         const fileName = resolveFile(inputs.method);
         const filePath = path.resolve(path.dirname(constants.CONFIG_PATH), fileName);
         if (!fs.existsSync(filePath)) {
-            console.log(`[netlify dotenv] Warning: Could not find ENV file '${filePath}'`);
+            log(`[netlify dotenv] Warning: Could not find ENV file '${filePath}'`);
             return;
         }
-        dotenv.config({
-            path: filePath,
-            encoding: 'utf8',
-        });
+        log(`[netlify dotenv] Log: Loading ENV file '${filePath}'`);
+        const output = dotenv.parse(fs.readFileSync(filePath, 'utf8'));
+        for (const [key, value] of Object.entries(output)) {
+            log(`[netlify dotenv] Log: Setting ${key} as ${value}`);
+            process.env[key] = value;
+        }
     }
 };
